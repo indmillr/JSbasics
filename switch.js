1d4351d6bdf424f2e03dbd2252c02261d2174373
Ian Miller <theiandmiller@gmail.com>
2021-11-22 20:58:02 -0600

    switch statement

---
+++ switch.js
@@ -0,0 +1,20 @@
+// selecting many options with Switch Statements
+
+function caseInSwitch(val) {
+  let answer = "";
+  Case (val === 1)
+    answer = "alpha";
+    break;
+  Case (val === 2)
+    answer = "beta";
+    break;
+  Case (val === 3)
+    answer = "gamma";
+    break;
+  Case (val === 4)
+    answer = "delta";
+    break;
+  return answer;
+};
+
+caseInSwitch(1);
