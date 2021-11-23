106e7a11dbaaa4e1755fdebc7f51f65ad38b05cb
Ian Miller <theiandmiller@gmail.com>
2021-11-23 06:30:29 -0600

    simple object property lookup function

--- playground.js
+++ playground.js
@@ -1,34 +1,15 @@
-let count = 0;
+let lookup = {
+  alpha: "Adams",
+  bravo: "Boston",
+  charlie: "Chicago",
+  delta: "Denver",
+  echo: "Easy",
+  foxtrot: "Frank",
+};
 
-function cc(card) {
-  switch (card) {
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 6:
-      count++;
-      break;
-    case 7:
-    case 8:
-    case 9:
-      break;
-    case 10:
-    case "J":
-    case "Q":
-    case "K":
-    case "A":
-      count--;
-  }
-  if (count > 0) {
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
+function phoneticLookup(val) {
+  let result = lookup[val];
+  return result;
 }
 
-cc(2);
-cc(3);
-cc(7);
-cc("K");
-cc("A");
+phoneticLookup("charlie");
