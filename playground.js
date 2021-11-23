16684cefbd1128b6794e8e0cabdbc86dea1468ce
Ian Miller <theiandmiller@gmail.com>
2021-11-23 06:59:44 -0600

    working through a function to update a Record Collection

--- playground.js
+++ playground.js
@@ -1,15 +1,34 @@
-let lookup = {
-  alpha: "Adams",
-  bravo: "Boston",
-  charlie: "Chicago",
-  delta: "Denver",
-  echo: "Easy",
-  foxtrot: "Frank",
+const recordCollection = {
+  2548: {
+    albumTitle: "Slippery When Wet",
+    artist: "Bon Jovi",
+    tracks: ["Let It Rock", "You Give Love a Bad Name"],
+  },
+  2468: {
+    albumTitle: "1999",
+    artist: "Prince",
+    tracks: ["1999", "Little Red Corvette"],
+  },
+  1245: {
+    artist: "Robert Palmer",
+    tracks: [],
+  },
+  5439: {
+    albumTitle: "ABBA Gold",
+  },
 };
 
-function phoneticLookup(val) {
-  let result = lookup[val];
-  return result;
+function updateRecords(records, id, prop, value) {
+  if (prop != "tracks" && value != "") {
+    recordCollection[id].prop = value;
+  } else if (prop === "tracks" && !updateRecords.hasOwnProperty("tracks")) {
+    recordCollection[id].tracks = [value];
+  } else if (prop === "tracks" && !value == "") {
+    recordCollection[id].tracks.push(value);
+  } else if (value == "") {
+    delete recordCollection[id].prop;
+  }
+  return records;
 }
 
-phoneticLookup("charlie");
+updateRecords(recordCollection, 5439, "artist", "ABBA");
