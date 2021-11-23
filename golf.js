38b4bcc8baa0acd7aae0639144128c6bbb2a042c
Ian Miller <theiandmiller@gmail.com>
2021-11-23 05:43:41 -0600

    a quick Golf Score function

--- golf.js
+++ golf.js
@@ -9,20 +9,20 @@ const names = [
 ];
 
 function golfScore(par, strokes) {
-  if (strokes >= par + 3) {
-    return names[6];
-  } else if (strokes == par + 2) {
-    return names[5];
-  } else if (strokes == par + 1) {
-    return names[4];
-  } else if (strokes == par) {
-    return names[3];
+  if (strokes === 1) {
+    return names[0];
+  } else if (strokes <= par - 2) {
+    return names[1];
   } else if (strokes == par - 1) {
     return names[2];
-  } else if (strokes == par - 2) {
-    return names[1];
+  } else if (strokes == par) {
+    return names[3];
+  } else if (strokes == par + 1) {
+    return names[4];
+  } else if (strokes == par + 2) {
+    return names[5];
   } else {
-    return names[0];
+    return names[6];
   }
 }
 
